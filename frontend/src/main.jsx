689483178cd5238c92ea0a7df49c807eead57518
Ram Potham <ram.potham@gmail.com>
2023-05-11 21:08:15 -0400

    Using # based routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,20 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 import TreeAnimationPage from "./TreeAnimationPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path=":address" element={<TreeAnimationPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Switch>
+        <Route path="/:address">
+          <TreeAnimationPage />
+        </Route>
+        <Route exact path="/">
+          <App />
+        </Route>
+      </Switch>
+    </HashRouter>
   </React.StrictMode>
 );
